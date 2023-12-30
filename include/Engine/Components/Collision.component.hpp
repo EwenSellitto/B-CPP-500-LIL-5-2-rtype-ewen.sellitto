74e2f87ae7de8696a455fa782954dded11f6a82b
FlorianMinguet <florian.minguet@epitech.eu>
2023-12-30 22:27:56 +0100

    feat(game-assets): Create a map with sprite name and sprites infos

--- include/Engine/Components/Collision.component.hpp
+++ include/Engine/Components/Collision.component.hpp
@@ -7,6 +7,9 @@
 
 #pragma once
 
+#include <map>
+#include <string>
+
 #include <SFML/Graphics/RenderTexture.hpp>
 #include <SFML/System/Vector2.hpp>
 
@@ -27,3 +30,39 @@ namespace Engine::Components
             sf::FloatRect rect;
     };
 } // namespace Engine::Components
+
+struct SpriteInfo {
+    std::string filePath;
+    int width;
+    int height;
+    int oneframeX;
+    int oneframeY;
+    int nbFrames;
+    int offsetX;
+    int offsetY;
+};
+
+class SpriteData
+{
+    public:
+        static std::map<std::string, SpriteInfo> spriteInfoMap;
+
+        static void loadSprites()
+        {
+            // Environnement
+            spriteInfoMap["Asteroid-Flame"] = {"../../assets/Environnement/assets/Environnement/Asteroid-Flame.png", 288, 96, 96, 96, 3, 36, 30};
+            spriteInfoMap["Asteroid01-Base"] = {"../../assets/Environnement/assets/Environnement/Asteroid01-Base.png", 96, 96, 96, 96, 1, 28, 31};
+
+            // klaed
+            spriteInfoMap["Klaed-Battlecruiser-Base"] = {"../../assets/klaed/assets/klaed/Klaed-Battlecruiser-Base.png", 128, 128, 128, 128, 1, 27, 12};
+            spriteInfoMap["Klaed-Battlecruiser-Destruction"] = {"../../assets/klaed/assets/klaed/Klaed-Battlecruiser-Destruction.png", 1792, 128, 128, 128, 14, 29, 14};
+            spriteInfoMap["Klaed-Bullet"] = {"../../assets/klaed/assets/klaed/Klaed-Bullet.png", 16, 16, 4, 16, 4, 1, 4};
+
+            // mainShip
+            spriteInfoMap["MainShip-Base-Fullhealth"] = {"../../assets/MainShip/assets/MainShip/MainShip-Base-Fullhealth.png", 48, 48, 48, 48, 1, 9, 11};
+            spriteInfoMap["MainShip-Base-Destruction"] = {"../../assets/MainShip/assets/MainShip/MainShip-Base-Destruction.png", 672, 48, 48, 48, 14, 9, 11};
+
+            // MainShipWeapons
+            spriteInfoMap["MainshipWeapon-Projectile-Autocannonbullet"] = {"../../assets/MainShipWeapons/assets/MainShipWeapons/MainshipWeapon-Projectile-Autocannonbullet.png", 128, 32, 32, 32, 4, 15, 12};
+        }
+};
